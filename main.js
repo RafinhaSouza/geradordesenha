521166f5fedbc895a4a321547139833584e35dc7
Rafaelly Souza <104680769+RafinhaSouza@users.noreply.github.com>
2024-09-27 22:04:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+constnumeroSenha = document.querySelector('.parametro-senha_texto');
+lettamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+constletrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+constletrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+constnumeros = '0123456789';
+constsimbolos = '!@%*?';
+constbotoes = document.querySelectorAll('.parametro-senha_botao');
+constcampoSenha = document.querySelector('#campo-senha');
+constcheckbox = document.querySelectorAll('.checkbox');
+constforcaSenha = document.querySelector('.forca');
+
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+
+functiondiminuiTamanho() {
+    if (tamanhoSenha>1){
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+functionaumentaTamanho(){
+    if  (tamanhoSenha<20){
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha(); 
+}
